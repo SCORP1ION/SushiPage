1db9c30fefd8567b0b07a3fc7434c308083d530b
SCORP1ION <scorpionip01010@gmail.com>
2026-06-24 14:10:01 -0600

    Fixed responsive to header movil

--- app/Section/Header/page.tsx
+++ app/Section/Header/page.tsx
@@ -1,11 +1,20 @@
+"use client"
 import styles from "./page.module.css"
+import { useState } from "react";
 
 export default function Header() {
+    const [menuOpen, setMenuOpen] = useState(false);
+    
     return (
         <>
             <header className={styles.container}>
                 <h1 className={styles.nameRestaurant}>OMAKASE</h1>
-                <nav className={styles.navContainer}>
+                <button className={styles.menuBottom}
+                    onClick={() => setMenuOpen(!menuOpen)}
+                >
+                    {menuOpen ? "X" : "☰"}
+                </button>
+                <nav className={`${styles.navContainer} ${menuOpen ? styles.active : "" }`}>
                     <a href="" className={styles.navA}>Home</a>
                     <a href="" className={styles.navA}>Menu</a>
                     <a href="" className={styles.navA}>About</a>
